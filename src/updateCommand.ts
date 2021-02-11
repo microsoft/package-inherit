a174bc56b7ddb6b014d12e5398901e96bf15ca88
Nick Palmer <nick@nick.codes>
2021-02-11 13:51:11 -0600

    Don't assume the target package has the key

--- src/updateCommand.ts
+++ src/updateCommand.ts
@@ -12,7 +12,13 @@ export function updateCommand(cwd: string) {
       const { packageJsonPath, ...output } = info;
 
       const newLine = detectNewline(fs.readFileSync(info.packageJsonPath, 'utf-8')) || os.EOL;
-
+      // Sort dependencies before outputing them like package managers do
+      ['dependencies', 'devDependencies'].map(function(key){
+        if (output[key]) {
+          const keys = Object.keys(output[key]).sort();
+          output[key] = JSON.parse(JSON.stringify(output[key], keys));
+        }
+      });
       fs.writeFileSync(
         info.packageJsonPath,
         JSON.stringify(output, null, 2).replace(/\n/g, newLine) + newLine
